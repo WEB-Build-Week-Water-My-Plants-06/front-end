df02b73b70cabe440921c68273bee32c4e7299a6
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-02 09:06:35 -0800

    Created profile form and connected. Also adjusted Logout.

--- water-my-plants/src/components/AddPlantForm.js
+++ water-my-plants/src/components/AddPlantForm.js
@@ -2,15 +2,14 @@ import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
-const initialValues = {
-  nickname: '',
-  species: '',
-  h20_freq: '',
-  plant_image: '',
-  user_id: localStorage.id,
-};
-
 export default function AddPlantForm() {
+  const initialValues = {
+    nickname: '',
+    species: '',
+    h20_freq: '',
+    plant_image: '',
+    user_id: localStorage.id,
+  };
   const [values, setValues] = useState (initialValues);
   const { push } = useHistory();
 
@@ -41,24 +40,22 @@ export default function AddPlantForm() {
       </div>
       <div className="formGroup">
         <div className="username-input form-spacing">
-          <label>
-            Nickname/Name
+          <label>Nickname/Name: </label>
             <input name="nickname" type="text" onChange={handleChange} />
-          </label>
         </div>
 
         <div className="password-input form-spacing">
-          <label>Species</label>
+          <label>Species: </label>
           <input name="species" type="text" onChange={handleChange} />
         </div>
 
         <div className="password-input form-spacing">
-          <label>Water Frequency</label>
+          <label>Water Frequency: </label>
           <input name="h20_freq" type="text" onChange={handleChange} />
         </div>
 
         <div className="password-input form-spacing">
-          <label>Image URL</label>
+          <label>Image URL: </label>
           <input name="plant_image" type="text" onChange={handleChange} />
         </div>
 
